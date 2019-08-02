4f923dad936fd5f164e2eda5d37e9c22d6bb15e8
Egle <egle.katilauskaite@gmail.com>
2019-08-02 15:48:11 +0100

    hexagon expands

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var screenWidth = screen.width;
 var screenHeight = screen.height;
 
-var draw = SVG('drawing').size(screenWidth, screenHeight);
-var polygon = draw.polygon('850,75  958,137.5 958,262.5 850,325 742,262.6 742,137.5').fill('lime').stroke({ width: 1 })
+// var draw = SVG('drawing').size(screenWidth, screenHeight);
+// var polygon = draw.polygon('850,75  958,137.5 958,262.5 850,325 742,262.6 742,137.5')
+//     .fill('images/test.png')
+//     .stroke({ width: 3 })
+//     .addClass('hexagon');
+
+// polygon.mouseover(function(){
+//     console.log("mouseover");
+// });
