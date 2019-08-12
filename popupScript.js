ac30fa79186375be6fc9e150fb2f17467b270f02
Egle <egle.katilauskaite@gmail.com>
2019-08-12 20:29:39 +0100

    change dataset

--- popupScript.js
+++ popupScript.js
@@ -4,35 +4,41 @@ var popup = $(".popup");
 var togglePopup = function(projectInfo) {
 
 
-    //popup is disabled
-    if (popedUpProjectHexagon === 0) {
-        updatePopup(projectInfo);
-        //enable popup
-        popup.toggle("drop", { "direction": "right" }, 500);
-        popedUpProjectHexagon = projectInfo.hexagon;
-    }
-    //popup is enabled
-    else {
-        //the same project
-        if (popedUpProjectHexagon === projectInfo.hexagon) {
-            popedUpProjectHexagon = 0;
-        }
-        //different project
-        else {
-
-            //quickly disable old one
-            popup.toggle("drop", { "direction": "right" }, 200);
-            popedUpProjectHexagon = projectInfo.hexagon;
+    if (projectInfo !== undefined && projectInfo !== null) {
+        //popup is disabled
+        if (popedUpProjectHexagon === 0) {
             updatePopup(projectInfo);
-
+            //enable popup
+            popup.toggle("drop", { "direction": "right" }, 500);
+            popedUpProjectHexagon = projectInfo.hexagon;
         }
-        //enable/disable
-        popup.toggle("drop", { "direction": "right" }, 500);
-    }
+        //popup is enabled
+        else {
+            //the same project
+            if (popedUpProjectHexagon === projectInfo.hexagon) {
+                popedUpProjectHexagon = 0;
+            }
+            //different project
+            else {
 
-    console.log(projectInfo);
+                //quickly disable old one
+                popup.toggle("drop", { "direction": "right" }, 200);
+                popedUpProjectHexagon = projectInfo.hexagon;
+                updatePopup(projectInfo);
+
+            }
+            //enable/disable
+            popup.toggle("drop", { "direction": "right" }, 500);
+        }
+    } else {
+        popup.toggle("drop", { "direction": "right" }, 500);
+        popedUpProjectHexagon = 0;
+    }
 };
 
+var back = function() {
+    togglePopup();
+};
 var updatePopup = function(projectInfo) {
 
     //change the title
@@ -46,24 +52,32 @@ var updatePopup = function(projectInfo) {
 
 
     //add other info
-    var popupInfo = $(".popupInfo");
+    var popupInfo = document.getElementsByClassName("popupInfo")[0];
+    popupInfo.innerHTML = "";
+
     for (var i = 0; i < projectInfo.shortDescriptions.length; i++) {
-        popupInfo.empty();
-        popupInfo.append("<h3>" + projectInfo.shortDescriptions[i].title + "</h3>");
-        popupInfo.append(projectInfo.shortDescriptions[i].description);
+        //create new div
+        var newDiv = document.createElement("div");
+        newDiv.className = ("popupDescription");
+        //create heading
+        var heading = document.createElement('h3');
+        heading.innerHTML = projectInfo.shortDescriptions[i].title;
+        //add heading to div
+        newDiv.appendChild(heading);
+        //add text to div
+        var node = document.createTextNode(projectInfo.shortDescriptions[i].description);
+        newDiv.appendChild(node);
+        //append div to popupinfo
+        popupInfo.appendChild(newDiv);
 
     }
 
     //if there's no link - remove the link
-    if (projectInfo.link === undefined || projectInfo.link === "") {
-        popup.find("a").hide();
+    if (projectInfo.link === undefined || projectInfo.link === "" || projectInfo.link === null) {
+        $(".externalProjectLink").hide();
     } else {
-        popup.find("a").show();
+        $(".externalProjectLink").show();
         //add link src
-        popup.find("a").attr("href", projectInfo.link);
+        $(".externalProjectLink").attr("href", projectInfo.link);
     }
-}
-
-var changePopupImage = function() {
-    $(".popupImage").attr("src", "second.jpg");
 };
