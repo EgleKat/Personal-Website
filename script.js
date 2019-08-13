cd0fb74c6b3a4983642c19c98859aa724d7dd3de
Egle <egle.katilauskaite@gmail.com>
2019-08-13 18:00:30 +0100

    svg repaints on resize

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
-var windowWidth = $(window).width();
-var windowHeight = $(window).height();
-var maxNoHorizontalHexagons = 4;
-var maxNoVerticalHexagons = 3;
-
-
-
-
-
-
+var windowWidth;
+var windowHeight;
 var draw;
-$(document).ready(function() {
 
-    $.getJSON("data/projects.json", function(data) {
-        //sort the data by the hexagon number
-        data.sort((a, b) => (a.hexagon > b.hexagon) ? 1 : -1);
-        console.log(data);
+function redrawHexagons(data) {
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+    draw.size(windowWidth, windowHeight);
 
-
-
-        draw = SVG('drawing').size(windowWidth, windowHeight);
-
-        var marginAmount = 200;
-
-        var adjustedWindowWidth = windowWidth - marginAmount;
-        var adjustedWindowHeight = windowHeight - marginAmount;
+    //sort the data by the hexagon number
+    data.sort((a, b) => (a.hexagon > b.hexagon) ? 1 : -1);
+    console.log(data);
 
 
-        /* ****** */
-        //these hexagons are pointy side up
-        var hexagonWidth = adjustedWindowWidth / 6.0; //middle row has 6 hexagons and fills width
-        var hexagonHeight = hexagonWidth / Math.sqrt(3) * 2; // https://www.redblobgames.com/grids/hexagons/
+    var marginAmount = 200;
 
-        // or
-        var hexagonHeight2 = adjustedWindowHeight / 2.5;
-        var hexagonWidth2 = hexagonHeight2 / 2.0 * Math.sqrt(3);
+    var adjustedWindowWidth = windowWidth - marginAmount;
+    var adjustedWindowHeight = windowHeight - marginAmount;
 
-        if (hexagonHeight2 < hexagonHeight) {
-            hexagonWidth = hexagonWidth2;
-            hexagonHeight = hexagonHeight2;
+
+    /* ****** */
+    //these hexagons are pointy side up
+    var hexagonWidth = adjustedWindowWidth / 6.0; //middle row has 6 hexagons and fills width
+    var hexagonHeight = hexagonWidth / Math.sqrt(3) * 2; // https://www.redblobgames.com/grids/hexagons/
+
+    // or
+    var hexagonHeight2 = adjustedWindowHeight / 2.5;
+    var hexagonWidth2 = hexagonHeight2 / 2.0 * Math.sqrt(3);
+
+    if (hexagonHeight2 < hexagonHeight) {
+        hexagonWidth = hexagonWidth2;
+        hexagonHeight = hexagonHeight2;
+    }
+
+    var smallRow = false; //small rows have 5 hexagons, large rows have 6
+    var numRows = 3;
+    var largeRowSize = 6;
+    var allHexagonsWidth = hexagonWidth * largeRowSize;
+    var columnOffset = (allHexagonsWidth + 0.01 < adjustedWindowWidth ? (adjustedWindowWidth - allHexagonsWidth) / 2.0 : 0);
+    var currentHexagon = 0;
+
+    for (var row = 0; row < numRows; row++) {
+        for (var column = 0; column < (smallRow ? largeRowSize - 1 : largeRowSize); column++) {
+            currentHexagon++;
+            var project = data.find(function (obj) {
+                return obj.hexagon === currentHexagon;
+            });
+            drawHexagon(project, column * hexagonWidth + (smallRow ? hexagonWidth : 0.5 * hexagonWidth) + columnOffset + marginAmount / 2, 0.75 * row * hexagonHeight + 0.5 * hexagonHeight + marginAmount / 2, hexagonWidth, hexagonHeight);
         }
+        smallRow = !smallRow;
+    }
+}
 
-        var smallRow = false; //small rows have 5 hexagons, large rows have 6
-        var numRows = 3;
-        var largeRowSize = 6;
-        var allHexagonsWidth = hexagonWidth * largeRowSize;
-        var columnOffset = (allHexagonsWidth + 0.01 < adjustedWindowWidth ? (adjustedWindowWidth - allHexagonsWidth) / 2.0 : 0);
-        var currentHexagon = 0;
 
-        for (var row = 0; row < numRows; row++) {
-            for (var column = 0; column < (smallRow ? largeRowSize - 1 : largeRowSize); column++) {
-                currentHexagon++;
-                var project = data.find(function(obj) {
-                    return obj.hexagon === currentHexagon;
-                });
-                drawHexagon(project, column * hexagonWidth + (smallRow ? hexagonWidth : 0.5 * hexagonWidth) + columnOffset + marginAmount / 2, 0.75 * row * hexagonHeight + 0.5 * hexagonHeight + marginAmount / 2, hexagonWidth, hexagonHeight);
-            }
-            smallRow = !smallRow;
-        }
+$(document).ready(function () {
+    draw = SVG('drawing').size(windowWidth, windowHeight);
 
-        /* ****** */
+    $.getJSON("data/projects.json", function (data) {
+
+
+        redrawHexagons(data);
+
+        $(window).resize(function () {
+            window.requestAnimationFrame(function () {
+                while (draw.node.firstChild) {
+                    draw.node.removeChild(draw.node.firstChild);
+                }
+                redrawHexagons(data);
+            });
+        });
     });
 
 });
@@ -80,7 +89,7 @@ function drawHexagon(projectInfo, cx, cy, w, h) {
     var scale = 1.5;
     var points = "";
     var scaledPoints = "";
-    var f = function(x, y) {
+    var f = function (x, y) {
         x = Math.round(x);
         y = Math.round(y);
         var scaledX = Math.round(scale * (x - cx) + cx);
@@ -132,7 +141,7 @@ function drawHexagon(projectInfo, cx, cy, w, h) {
     }
     //there is a project for this hexagon 
     else {
-        var pattern = draw.pattern(1, 1, function(add) {
+        var pattern = draw.pattern(1, 1, function (add) {
             add.rect(2 * w, 2 * h).fill("#fff");
             var i = add.image("images/" + projectInfo.image);
             i.attr({
@@ -141,7 +150,7 @@ function drawHexagon(projectInfo, cx, cy, w, h) {
                 width: 4 * w,
                 height: 4 * h
             });
-            polygon.mouseover(function() {
+            polygon.mouseover(function () {
                 i.animate(200).attr({
                     x: -0.25 * w,
                     y: -0.25 * h,
@@ -149,7 +158,7 @@ function drawHexagon(projectInfo, cx, cy, w, h) {
                     height: 2 * h
                 });
             });
-            polygon.mouseout(function() {
+            polygon.mouseout(function () {
                 i.animate(200).attr({
                     x: -1.5 * w,
                     y: -1.5 * h,
@@ -164,21 +173,21 @@ function drawHexagon(projectInfo, cx, cy, w, h) {
             patternContentUnits: "userSpaceOnUse",
         });
         polygon
-        //change hexagon image
-        // .fill('url(#image1)')
+            //change hexagon image
+            // .fill('url(#image1)')
             .fill(pattern)
             .attr("class", "hexagon active");
         //.fill(draw.image("images/" + projectInfo.image, 500, 500));
-        polygon.click(function() {
+        polygon.click(function () {
             togglePopup(projectInfo);
         });
 
     }
-    polygon.mouseover(function() {
+    polygon.mouseover(function () {
         polygon.front();
         document.getElementById(animateMouseover.id()).beginElement();
     });
-    polygon.mouseout(function() {
+    polygon.mouseout(function () {
         document.getElementById(animateMouseout.id()).beginElement();
     });
 
